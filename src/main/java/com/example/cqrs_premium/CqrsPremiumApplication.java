b8b3dadc1665d0478894c2977c771fd2431908ad
plan11plan <tjtfy7028@gmail.com>
2024-09-16 14:25:23 +0900

    first commit

---
+++ src/main/java/com/example/cqrs_premium/CqrsPremiumApplication.java
@@ -0,0 +1,13 @@
+package com.example.cqrs_premium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsPremiumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CqrsPremiumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cqrs_premium/CqrsPremiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cqrs_premium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsPremiumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
